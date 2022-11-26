5bb95f699757632ab3f9345edee59fc113ee5c3f
sendnoose <alti0204@gmail.com>
2022-11-26 17:17:52 +0700

    trash thing

--- Assets/Scripts/MainGame/MovingObstacle.cs
+++ Assets/Scripts/MainGame/MovingObstacle.cs
@@ -7,10 +7,24 @@ public class MovingObstacle : MonoBehaviour
     [SerializeField] private float _speed = 10f;
     [SerializeField] private Transform _startPosition;
     [SerializeField] private Transform _endPosition;
+    [SerializeField] private float _startDelay = 0.5f;
+    [SerializeField] private float _playerBlowUpDelay = 1f;
+
+    private float _idleTimer;
+
+    public float IdleTimer
+    {
+        get { return _idleTimer; }
+        set
+        {
+            _idleTimer = value;
+            gameObject.SetActive(false);
+        }
+    }
 
     public void StartMoving()
     {
-
+        transform.position = _startPosition.position;
     }
 
     private void Start()
@@ -20,14 +34,24 @@ public class MovingObstacle : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (gameObject.activeSelf)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _endPosition.position, _speed * Time.fixedDeltaTime);
         
+        if (Vector2.Distance(transform.position, _endPosition.position) < 0.1f)
+        {
+            StartMoving();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Player"))
         {
-            gameObject.SetActive(false);
+            IdleTimer = _playerBlowUpDelay;
         }
     }
 }
