5a218ed3c881584f76731aba5166b1513d1be483
FinisTP <tanphat.tu_intern@ved.com.vn>
2022-11-26 17:37:06 +0700

    Added map 1 temp

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,13 @@
+namespace Dialogue {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System;
+    using TMPro;
+
+    public class DialogueManager : MonoBehaviour
+    {
+        public Dictionary<string, DialogueManager> dialogueDictionary;
+    }
+}
+
